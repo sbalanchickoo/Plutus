bd7f6e0ca0790e8277012c2b486b49a0bb255fcd
Srikant Balan VMWin10K <balansrikant@gmail.com>
2018-11-27 16:19:50 +0000

    Removed extra usings

--- src/Plutus.UI.Shared.CS/Models/Merchant.cs
+++ src/Plutus.UI.Shared.CS/Models/Merchant.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Plutus.UI.Shared.CS.Models
+﻿namespace Plutus.UI.Shared.CS.Models
 {
     public class Merchant
     {
